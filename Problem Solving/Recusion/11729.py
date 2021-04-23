77ee917b52bed67eac6c8cdaa70359e8d2614768
DogonYu <dorogon01@gmail.com>
2021-04-23 22:27:30 +0900

    TIL 2021-04-23 백준 하노이탑 재귀

---
+++ Problem Solving/Recusion/11729.py
@@ -0,0 +1,15 @@
+# 재귀 - 하노이 탑
+
+import sys
+
+def hanoi(start, end, n):
+  if n == 0:
+    return
+  hanoi(start, 6-start-end, n-1)
+  print(start, end)
+  hanoi(6-start-end, end, n-1)
+
+n = int(sys.stdin.readline())
+
+print(2 ** n - 1)
+hanoi(1, 3, n)
